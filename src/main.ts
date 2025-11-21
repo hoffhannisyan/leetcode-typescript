61b7c26fb06751df9fbfc33b14a0af430ee1b38b
hoffhannisyan <hoffhannisyan@gmail.com>
2025-11-21 10:16:28 +0400

    feat: add solution for problem #1 Two Sum

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log("TypeScript setup is working correctly!");
+import { twoSum } from "@/001-100/0001-two-sum.js";
+
+console.log("=== #1 Two Sum ===");
+console.log("Example 1:", twoSum([2, 7, 11, 15], 9));
+console.log("Example 2:", twoSum([3, 2, 4], 6));
+console.log("Example 3:", twoSum([3, 3], 6));
